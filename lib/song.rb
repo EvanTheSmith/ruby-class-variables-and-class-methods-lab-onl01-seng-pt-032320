4d86c2e0af2a76bdef10db321cef6e7a00e39d0c
Evan Smith <professionalemail@zoho.com>
2020-04-10 17:16:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,12 @@
 class Song 
-  @@song_count = 0 
+  @@count = 0 
+  @@total_artists = []
+  @@total_genres = []
+  @@genre_count = {}
+  @@artist_count = {}
+  
+  attr_accessor :name, :artist, :genre
+  
   
   
 end
